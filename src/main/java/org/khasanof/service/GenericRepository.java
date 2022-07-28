8212fee54051845f7141cb26db4f410d00cda60d
Nurislom373 <nurislomx72@gmail.com>
2022-07-28 18:42:35 +0500

    GenericRepository.java add delete method

--- src/main/java/org/khasanof/service/GenericRepository.java
+++ src/main/java/org/khasanof/service/GenericRepository.java
@@ -9,10 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class GenericRepository<T, ID> {
     private final Connection connection = ConnectionConfig.getConnection();
@@ -24,58 +21,104 @@ public class GenericRepository<T, ID> {
         this.persistenceClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         this.objectMapper = new ObjectMapper();
         this.genericUtils = new GenericUtils();
+        checkTable();
     }
 
-    public T find(ID id) {
+    public T getById(ID id) {
         try {
-            if (tableExist(persistenceClass.getSimpleName().toLowerCase(Locale.ROOT))) {
-                PreparedStatement preparedStatement = connection.prepareStatement("select * from " + persistenceClass.getSimpleName() + " where id = " + id);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    Object o = genericUtils.get(resultSet, getFields());
-                    System.out.println("o = " + o);
-                    return objectMapper.convertValue(o, persistenceClass);
-                }
-            } else {
-                createTable();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from " + persistenceClass.getSimpleName() + " where id = " + id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Object o = genericUtils.get(resultSet, getFields());
+                System.out.println("o = " + o);
+                return objectMapper.convertValue(o, persistenceClass);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Optional<T> findById(ID id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from " + persistenceClass.getSimpleName() + " where id = " + id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Object o = genericUtils.get(resultSet, getFields());
+                System.out.println("o = " + o);
+                return Optional.of(objectMapper.convertValue(o, persistenceClass));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 
     public List<T> findAll() {
         try {
-            if (tableExist(persistenceClass.getSimpleName().toLowerCase(Locale.ROOT))) {
-                List<T> list = new ArrayList<>();
-                PreparedStatement preparedStatement = connection.prepareStatement("select * from " + persistenceClass.getSimpleName());
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    list.add(new ObjectMapper().convertValue(genericUtils.get(resultSet, getFields()), persistenceClass));
-                }
-                return list;
-            } else {
-                createTable();
+            List<T> list = new ArrayList<>();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from " + persistenceClass.getSimpleName());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                list.add(new ObjectMapper().convertValue(genericUtils.get(resultSet, getFields()), persistenceClass));
             }
+            return list;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public T save(T entity) {
+    public void save(T entity) {
         try {
-            if (tableExist(persistenceClass.getSimpleName().toLowerCase(Locale.ROOT))) {
-                connection.createStatement().execute(insertQuery(entity));
-            } else {
-                createTable();
+            connection.createStatement().execute(insertQuery(entity));
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(List<T> tList) {
+        try {
+            for (T entity : tList) {
                 connection.createStatement().execute(insertQuery(entity));
             }
         } catch (SQLException | IllegalAccessException e) {
             e.printStackTrace();
         }
-        return null;
+    }
+
+    public void delete(T entity) {
+        try {
+            connection.createStatement().execute(deleteQuery(entity));
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteById(ID id) {
+        try {
+            connection.createStatement().execute(deleteByIdQuery(id));
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAll() {
+        try {
+            connection.createStatement().execute(deleteAllQuery());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAll(List<T> tList) {
+        try {
+            for (T entity : tList) {
+                connection.createStatement().execute(deleteQuery(entity));
+            }
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -83,6 +126,44 @@ public class GenericRepository<T, ID> {
         return persistenceClass.getDeclaredFields();
     }
 
+    private String deleteByIdQuery(ID id) throws IllegalAccessException {
+        StringBuilder query = new StringBuilder("delete from " + persistenceClass.getSimpleName() + " where id = ");
+        if (isNumber(id.getClass().getName())) {
+            query.append(id);
+        } else {
+            query.append("'").append(id).append("'");
+        }
+        System.out.println(query);
+        return String.valueOf(query);
+    }
+
+    private String deleteAllQuery() {
+        return "truncate table " + persistenceClass.getSimpleName() + ";";
+    }
+
+    private String deleteQuery(T entity) throws IllegalAccessException {
+        StringBuilder query = new StringBuilder("delete from " + persistenceClass.getSimpleName() + " where ");
+        Field[] fields = entity.getClass().getDeclaredFields();
+        int count = 0;
+        for (Field field : fields) {
+            count++;
+            if ((Arrays.stream(fields).count() - count) == 0) {
+                if (isNumber(field.getGenericType().getTypeName())) {
+                    query.append(field.getName()).append(" = ").append(getValue(entity, field)).append(";");
+                } else {
+                    query.append(field.getName()).append(" = '").append(getValue(entity, field)).append("';");
+                }
+            } else {
+                if (isNumber(field.getGenericType().getTypeName())) {
+                    query.append(field.getName()).append(" = ").append(getValue(entity, field)).append(" and ");
+                } else {
+                    query.append(field.getName()).append(" = '").append(getValue(entity, field)).append("' and ");
+                }
+            }
+        }
+        System.out.println(query);
+        return String.valueOf(query);
+    }
 
     private String insertQuery(T entity) throws IllegalAccessException {
         StringBuilder query = new StringBuilder("insert into " + persistenceClass.getSimpleName() + " (");
@@ -94,9 +175,9 @@ public class GenericRepository<T, ID> {
                 continue;
             }
             if ((Arrays.stream(fields).count() - count) == 0) {
-                query.append(field.getName() + ") values (");
+                query.append(field.getName()).append(") values (");
             } else {
-                query.append(field.getName() + ", ");
+                query.append(field.getName()).append(", ");
             }
         }
         int secondCount = 0;
@@ -107,21 +188,52 @@ public class GenericRepository<T, ID> {
                 continue;
             }
             if ((Arrays.stream(declaredFields).count() - secondCount) == 0) {
-                if (declaredField.getGenericType().getTypeName().contains("Integer")) {
-                    query.append(getValue(entity, declaredField) + ");");
+                if (declaredField.getGenericType().getTypeName().contains("Integer") || declaredField.getGenericType().getTypeName().equals("int")) {
+                    query.append(getValue(entity, declaredField)).append(");");
                 } else {
-                    query.append("'" + getValue(entity, declaredField) + "');");
+                    query.append("'").append(getValue(entity, declaredField)).append("');");
                 }
             } else {
                 if (declaredField.getGenericType().getTypeName().contains("Integer")) {
-                    query.append(getValue(entity, declaredField) + ",");
+                    query.append(getValue(entity, declaredField)).append(",");
                 } else {
-                    query.append("'" + getValue(entity, declaredField) + "',");
+                    query.append("'").append(getValue(entity, declaredField)).append("',");
                 }
             }
         }
         System.out.println(query);
         return query.toString();
+    }
+
+    private Object getValue(T entity, Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        return field.get(entity);
+    }
+
+    private boolean isNumber(String type) {
+        return type.contains("Integer")
+                || type.contains("Float")
+                || type.contains("BigDecimal")
+                || type.contains("Short")
+                || type.contains("Byte")
+                || type.contains("Double")
+                || type.equals("int")
+                || type.equals("byte")
+                || type.equals("short")
+                || type.equals("double")
+                || type.equals("lon")
+                || type.equals("float");
+    }
+
+
+    private void checkTable() {
+        try {
+            if (!tableExist(persistenceClass.getSimpleName().toLowerCase(Locale.ROOT))) {
+                createTable();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private void createTable() throws SQLException {
@@ -134,20 +246,13 @@ public class GenericRepository<T, ID> {
             if (field.getName().equals("id")) {
                 query.append("id serial primary key,");
             } else if ((fieldsCount - count) == 0) {
-                query.append(" " + field.getName() + " " + genericUtils.dataTypeConvertToSQl(field.getGenericType()) + " );");
+                query.append(" ").append(field.getName()).append(" ").append(genericUtils.dataTypeConvertToSQl(field.getGenericType())).append(" );");
             } else {
-                query.append(" " + field.getName() + " " + genericUtils.dataTypeConvertToSQl(field.getGenericType()) + ",");
+                query.append(" ").append(field.getName()).append(" ").append(genericUtils.dataTypeConvertToSQl(field.getGenericType())).append(",");
             }
         }
         connection.createStatement().execute(query.toString());
     }
-
-
-    private Object getValue(T entity, Field field) throws IllegalAccessException {
-        field.setAccessible(true);
-        return field.get(entity);
-    }
-
 
     private boolean tableExist(String tableName) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) "
